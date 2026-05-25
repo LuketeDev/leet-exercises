32d2569d070ec03e3b7019caefb7b17c79684f90
LuketeDev <lulukas.2023@gmail.com>
2026-05-24 23:30:39 -0300

    score of a string better

--- arrays_and_hashing/easy/ScoreOfAString.java
+++ arrays_and_hashing/easy/ScoreOfAString.java
@@ -1,7 +1,9 @@
 package arrays_and_hashing.easy;
 
 public class ScoreOfAString {
+    // Solution 1 with O(N) speed and memory
     public int scoreOfString(String s) {
+        // Casting a char to an int returns it's ASCII value
         int[] arrAscii = s.chars().toArray();
         int res = 0;
         for (int i = 0; i < arrAscii.length - 1; i++) {
@@ -9,5 +11,15 @@ public class ScoreOfAString {
             res += sum;
         }
         return res;
+    }
+
+    // Solution 2 with O(1) memory
+    public int scoreOfStringTwo(String s) {
+        int score = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            // No need to cast, mathematical operators already consider ASCII
+            score += Math.abs(s.charAt(i) - s.charAt(i + 1));
+        }
+        return score;
     }
 }
